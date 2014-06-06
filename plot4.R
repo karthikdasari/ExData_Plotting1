de2150f3aed1747c09eee74ad59ea6e2ee1cb680
karthikdasari <karthikdasari@users.noreply.github.com>
2014-06-06 17:22:48 +0530

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,33 @@
+setwd("/Users/karthikdasari/Desktop/Coursera") # To set the working directory to the 
+# folder containing data
+options(stringsAsFactors = FALSE)
+
+data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
+
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y") # converting date to appropriate format
+
+datar <- data[(data$Date =="2007-02-01" | data$Date == "2007-02-02"),] # Subsetting the required data
+
+a <- strptime(paste(datar$Date, datar$Time), format = "%Y-%m-%d %H:%M:%S")
+
+png(file = "plot4.png", width = 480, height = 480, bg = "white") # opens the graphic device PNG
+
+datar <- cbind(datar,a)
+
+par(mfrow = c(2, 2))
+
+with(datar, {
+        plot(datar$a, as.numeric(datar$Global_active_power), type = "l", ylab = "Global Active Power" , xlab = "")
+        plot(datar$a, as.numeric(datar$Voltage), type = "l", ylab = "Voltage" , xlab = "datetime")
+        plot(datar$a, as.numeric(datar$Sub_metering_1),col="black", type="l",ylab = "Energy submetering", xlab = "")
+        
+        lines(datar$a,as.numeric(datar$Sub_metering_3), type="l",col="blue")
+        
+        lines(datar$a,as.numeric(datar$Sub_metering_2), type="l",col="red")
+        
+        legend("topright", pch = "-", col = c( "black","blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+        
+        plot(datar$a, as.numeric(datar$Global_reactive_power), type = "l", ylab = "Global_reactive_power" , xlab = "datetime")
+})
+
+dev.off()
