ffa2e9d71c5b0b5c38a74c058f810f071da81029
karthikdasari <karthikdasari@users.noreply.github.com>
2014-06-06 15:43:26 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -10,7 +10,7 @@ datar <- data[(data$Date =="2007-02-01" | data$Date == "2007-02-02"),] # Subsett
 
 datar$Global_active_power <- as.numeric(datar$Global_active_power# converting the data to numeric class
                                         
-png(file = "plot1.png" width = 480, height = 480, bg = "white") # opens the graphic device PNG with mentioned pixels on a white bacjground
+png(file = "plot1.png" width = 480, height = 480, bg = "white") # opens the graphic device PNG with mentioned pixels on a white background
 
 hist(datar$Global_active_power, col = "red", xlab = "Global Active Power (Kiloatts)", main = "Global Active Power")
 
